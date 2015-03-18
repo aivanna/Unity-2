829c79adc78c61ffe8454ba4b73b8307af58281b
aivanna <comp4@Mac-mini-comp4.local>
2015-03-18 09:20:15 +0200

    Txt
    документ

---
+++ cont.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cont : MonoBehaviour
+{
+		CharacterController controller;
+		Vector3 moveDirection;
+		int speed = 1;
+		int gravity = 10;				
+		int speedRotate = 2;
+
+		void Awake ()
+		{
+				controller = GetComponent<CharacterController> ();
+		//jiiuiuiuiu
+
+
+		}
+	
+
+		void Update ()
+		{
+
+				
+					
+				//}
+				
+				//moveDirection.y -= gravity * Time.deltaTime;		
+				controller.Move (moveDirection * Time.deltaTime);		
+				transform.Rotate (0, 0.3f, 0, Space.World);
+		}
+
+
+		
+}
